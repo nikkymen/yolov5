e85969610b60b14cd44b21fb2b8ec04872ffd954
nikita.kozhanov <nikita.kozhanov@kronshtadt.ru>
2021-11-01 13:07:55 +0300

    Fix issue with [img2label_paths]

--- utils/datasets.py
+++ utils/datasets.py
@@ -369,8 +369,8 @@ class LoadStreams:
 
 def img2label_paths(img_paths):
     # Define label paths as a function of image paths
-    sa, sb = os.sep + 'images' + os.sep, os.sep + 'labels' + os.sep  # /images/, /labels/ substrings
-    return [sb.join(x.rsplit(sa, 1)).rsplit('.', 1)[0] + '.txt' for x in img_paths]
+    return [x.replace('/images/', '/labels/', 1).rsplit('.', 1)[0] + '.txt' for x in img_paths]
+    #return [sb.join(x.rsplit(sa, 1)).rsplit('.', 1)[0] + '.txt' for x in img_paths]
 
 
 class LoadImagesAndLabels(Dataset):
